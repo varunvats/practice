8845674b7e558e471c5f629fdac42fb325267e51
Varun Vats <varun.vats@here.com>
2016-01-05 18:06:02 -0600

    Implement and test Quick sort.
    The implementation has the sentinel optimization.

---
+++ jvm/src/main/java/com/varunvats/practice/sorting/Quick.java
@@ -0,0 +1,58 @@
+package com.varunvats.practice.sorting;
+
+import edu.princeton.cs.algs4.StdRandom;
+
+public class Quick extends SortBase {
+
+    private Quick() {
+        // This class cannot be instantiated.
+    }
+
+    public static <T extends Comparable<T>> void sort(T[] a) {
+        final int N = a.length;
+        StdRandom.shuffle(a);
+        createSentinel(a);
+        sort(a, 0, N - 1);
+    }
+
+    private static <T extends Comparable<T>> void sort(T[] a, int low, int high) {
+        if (low >= high)
+            return;
+        final int j = partition(a, low, high);
+        sort(a, low, j - 1);
+        sort(a, j + 1, high);
+    }
+
+    private static <T extends Comparable<T>> int partition(T[] a, int low, int high) {
+        final T v = a[low];
+        int leftCursor = low;
+        int rightCursor = high + 1;
+        while (true) {
+            while (less(a[++leftCursor], v)) {
+            }
+            while (less(v, a[--rightCursor])) {
+            }
+            if (leftCursor >= rightCursor)
+                break;
+            exchange(a, leftCursor, rightCursor);
+        }
+        exchange(a, low, rightCursor);
+        return rightCursor;
+    }
+
+    private static <T extends Comparable<T>> void createSentinel(T[] a) {
+        final int N = a.length;
+        if (N > 1) {
+            final int maxValueIndex = getMaxValueIndex(a);
+            exchange(a, maxValueIndex, N - 1);
+        }
+    }
+
+    private static <T extends Comparable<T>> int getMaxValueIndex(T[] a) {
+        int maxValueIndex = 0;
+        for (int i = 1; i < a.length; i++)
+            if (less(a[maxValueIndex], a[i]))
+                maxValueIndex = i;
+        return maxValueIndex;
+    }
+}
---
+++ jvm/src/test/scala/com/varunvats/practice/sorting/QuickSpec.scala
@@ -0,0 +1,55 @@
+package com.varunvats.practice.sorting
+
+class QuickSpec extends UnitSpec {
+
+  "Quick sort" must {
+
+    "sort an empty array" in {
+      val a = array()
+      Quick.sort(a)
+      a shouldBe Array.empty[Double]
+    }
+
+    "sort an array containing only one element" in {
+      val a = array(5.3)
+      Quick.sort(a)
+      a shouldBe Array(5.3)
+    }
+
+    "sort an array containing two elements" in {
+      val a = array(9.1, 3.5)
+      Quick.sort(a)
+      a shouldBe Array(3.5, 9.1)
+    }
+
+    "sort an already sorted array" in {
+      val a = array(3, 9, 10.1, 21, 99, 1000.01)
+      Quick.sort(a)
+      a shouldBe Array(3, 9, 10.1, 21, 99, 1000.01)
+    }
+
+    "sort an array sorted in reverse order" in {
+      val a = array(100.99, 53, 21, 9.3, 2.1, 0.0)
+      Quick.sort(a)
+      a shouldBe Array(0.0, 2.1, 9.3, 21, 53, 100.99)
+    }
+
+    "sort an unsorted array containing even number of elements" in {
+      val a = array(5, 10, 9, 3, 0, 29)
+      Quick.sort(a)
+      a shouldBe Array(0, 3, 5, 9, 10, 29)
+    }
+
+    "sort an unsorted array containing odd number of elements" in {
+      val a = array(5, 10, 9, 3, 0, 29, 1)
+      Quick.sort(a)
+      a shouldBe Array(0, 1, 3, 5, 9, 10, 29)
+    }
+
+    "sort an array in which all the elements have the same value" in {
+      val a = array(5, 5, 5, 5, 5)
+      Quick.sort(a)
+      a shouldBe Array(5, 5, 5, 5, 5)
+    }
+  }
+}
