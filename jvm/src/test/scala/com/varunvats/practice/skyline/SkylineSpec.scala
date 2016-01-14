98d95b6554a0e06a10f88e17f4649f3c5089ec26
Varun Vats <varun.vats@live.com>
2016-01-14 01:08:05 -0600

    Add one more test (x != 0) for skyline problem.

--- jvm/src/test/scala/com/varunvats/practice/skyline/SkylineSpec.scala
+++ jvm/src/test/scala/com/varunvats/practice/skyline/SkylineSpec.scala
@@ -6,11 +6,14 @@ class SkylineSpec extends UnitSpec {
 
   "A correct skyline" must {
 
-    "be created for a single building located at (0, 0)" in {
+    "be created for a single building located at x = 0.0" in {
       val buildings = List(Building(0, 6.5, 21.3))
       Skyline.trace(buildings) shouldBe List((0.0, 0.0), (0.0, 21.3), (6.5, 21.3), (6.5, 0))
     }
 
+    "be created for a single building located at x = 11.1" in {
+      val buildings = List(Building(11.1, 19.5, 21.3))
+      Skyline.trace(buildings) shouldBe List((0.0, 0.0), (11.1, 0.0), (11.1, 21.3), (19.5, 21.3), (19.5, 0))
+    }
   }
-
 }
