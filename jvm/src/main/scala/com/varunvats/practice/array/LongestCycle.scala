912b783aaa8589086095b83960cf8fbc6ae323ab
Varun Vats <varun.vats@live.com>
2016-01-19 14:40:38 -0600

    Array Longest Cycle: add more tests.

--- jvm/src/main/scala/com/varunvats/practice/array/LongestCycle.scala
+++ jvm/src/main/scala/com/varunvats/practice/array/LongestCycle.scala
@@ -5,28 +5,23 @@ import scala.annotation.tailrec
 object LongestCycle {
 
   def get(arr: IndexedSeq[Int]): Seq[Int] = {
-    val (longestCycle, _) = arr.foldLeft((Seq.empty[Int], Set.empty[Int])) { case ((longestCycleSoFar, visited), start) =>
-      if (visited.contains(start))
-        (longestCycleSoFar, visited)
-      else {
-        val (newCycle, newVisited) = getNewCycle(arr, start, Nil, visited)
-        if (newCycle.length > longestCycleSoFar.length)
-          (newCycle, newVisited)
-        else
-          (longestCycleSoFar, newVisited)
-      }
+    val longestCycle = arr.foldLeft(Seq.empty[Int]) { (longestCycleSoFar, start) =>
+      val newCycle = getNewCycle(arr, start, Nil, Set.empty)
+      if (newCycle.length > longestCycleSoFar.length)
+        newCycle
+      else
+        longestCycleSoFar
     }
     longestCycle.reverse
   }
 
   @tailrec
-  private def getNewCycle(arr: IndexedSeq[Int], start: Int, pathSoFar: Seq[Int], visited: Set[Int]): (Seq[Int], Set[Int]) = {
+  private def getNewCycle(arr: IndexedSeq[Int], start: Int, pathSoFar: Seq[Int], visited: Set[Int]): Seq[Int] = {
     val newVisited = visited + start
     if (!arr.isDefinedAt(start))
-      (Nil, newVisited)
-    else if (pathSoFar.contains(start)) {
-      val cycle = getCycle(arr, start, Seq(start), arr(start))
-      (cycle, newVisited)
+      Nil
+    else if (visited.contains(start)) {
+      getCycle(arr, start, Seq(start), arr(start))
     } else {
       getNewCycle(arr, arr(start), start +: pathSoFar, newVisited)
     }
--- jvm/src/test/scala/com/varunvats/practice/array/LongestCycleSpec.scala
+++ jvm/src/test/scala/com/varunvats/practice/array/LongestCycleSpec.scala
@@ -25,14 +25,14 @@ class LongestCycleSpec extends UnitSpec {
         LongestCycle.get(IndexedSeq(0)) shouldBe Seq(0)
       }
     }
-    
+
     "two elements that don't form a cycle" should {
       "be empty" in {
         LongestCycle.get(IndexedSeq(1, 2)) shouldBe Nil
       }
     }
 
-    "two elements that form a cycle" should {
+    "two elements that both are in one cycle" should {
       "contain both the elements" in {
         LongestCycle.get(IndexedSeq(1, 0)) shouldBe Seq(1, 0)
       }
@@ -45,10 +45,40 @@ class LongestCycleSpec extends UnitSpec {
       }
     }
 
+    "three elements that are all part of the cycle (cycle direction is clockwise in array)" should {
+      "contain all the three elements in the correct order" in {
+        val arr = IndexedSeq(1, 2, 0)
+        LongestCycle.get(arr) shouldBe Seq(1, 2, 0)
+      }
+    }
+
     "four elements that are all part of the cycle (cycle direction is random in array)" should {
       "contain all the four elements in the correct order" in {
         val arr = IndexedSeq(2, 3, 1, 0)
         LongestCycle.get(arr) shouldBe Seq(2, 1, 3, 0)
+      }
+    }
+
+    "a cycle that starts in the middle of the array" should {
+      "contain all the elements in the cycle in the correct order" in {
+        val arr = IndexedSeq(99, 0, 4, 2, 3, 21, 63)
+        LongestCycle.get(arr) shouldBe Seq(4, 3, 2)
+      }
+    }
+
+    "two cycles of the same length" should {
+      "be the one encountered first from the left" in {
+        val arr = IndexedSeq(2, 6, 11, 10, 1, 0, 3, 8, 7, 5, 4, 9)
+        val expectedCycle = Seq(2, 11, 9, 5, 0)
+        LongestCycle.get(arr) shouldBe expectedCycle
+      }
+    }
+
+    "two cycles of different lengths" should {
+      "the one with more elements" in {
+        val arr = IndexedSeq(99, 6, 11, 10, 1, 2, 3, 8, 7, 5, 4, 9)
+        val expectedCycle = Seq(6, 3, 10, 4, 1)
+        LongestCycle.get(arr) shouldBe expectedCycle
       }
     }
   }
