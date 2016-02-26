4dda6103cbba0868c0f6d67b9dcf80ec882d4b76
Varun Vats <varun.vats@live.com>
2016-02-25 23:09:22 -0600

    Product of Other Elements in Array: Simplify implementation.

--- jvm/src/main/scala/com/varunvats/practice/array/ProductOtherElements.scala
+++ jvm/src/main/scala/com/varunvats/practice/array/ProductOtherElements.scala
@@ -4,49 +4,33 @@ import scala.annotation.tailrec
 
 object ProductOtherElements {
 
-  private case class Accum(accum: Int)
-
-  private case class ZIndex(index: Int)
-
   def apply(input: Array[Int]): Array[Int] = {
     if (input.length <= 1)
       return Array(input: _*)
-    val res = process(input, 0, (Accum(1), Option.empty[(Accum, ZIndex)]))
-    res match {
-      case (Accum(accum), None) if accum != 0 => // No zeroes in array.
-        input.map(value => accum / value)
-      case (_, None) => // More than one zero in array.
+    val (product1, firstZeroIndexO) = process(input, 0, 1)
+    firstZeroIndexO.fold(createOutputArray(input, product1)) { firstZeroIndex =>
+      val (product, secondZeroIndexO) = process(input, firstZeroIndex + 1, product1)
+      secondZeroIndexO.fold(createOutputArray(input.length, firstZeroIndex, product)) { _ =>
         Array.fill(input.length)(0)
-      case (_, Some((Accum(accum), ZIndex(zeroValueIndex)))) => // Only one zero in array.
-        val output = Array.fill(input.length)(0)
-        output(zeroValueIndex) = accum
-        output
+      }
     }
   }
 
   @tailrec
-  private def process(input: Array[Int],
-                      index: Int,
-                      result: (Accum, Option[(Accum, ZIndex)])): (Accum, Option[(Accum, ZIndex)]) = {
-    if (index == input.length)
-      return result
-    val nextIndex = index + 1
-    if (input(index) == 0)
-      result match {
-        case (Accum(accum), None) => // First zero.
-          val newResult = (Accum(0), Some(Accum(accum), ZIndex(index)))
-          process(input, nextIndex, newResult)
-        case (_, Some(_)) => // Second zero.
-          (Accum(0), None)
-      }
-    else
-      result match {
-        case (Accum(accum), None) => // No zeroes so far.
-          val newResult = (Accum(accum * input(index)), None)
-          process(input, nextIndex, newResult)
-        case (zero, Some((Accum(accum), zeroIndex))) => // One zero so far.
-          val newResult = (zero, Some(Accum(accum * input(index)), zeroIndex))
-          process(input, nextIndex, newResult)
-      }
+  private def process(arr: Array[Int], index: Int, acc: Int): (Int, Option[Int]) = {
+    if (index >= arr.length)
+      return (acc, None)
+    if (arr(index) == 0)
+      return (acc, Some(index))
+    process(arr, index + 1, acc * arr(index))
   }
+
+  private def createOutputArray(input: Array[Int], product: Int): Array[Int] = input.map(value => product / value)
+
+  private def createOutputArray(length: Int, zeroIndex: Int, product: Int): Array[Int] = {
+    val arr = Array.fill(length)(0)
+    arr(zeroIndex) = product
+    arr
+  }
+
 }
