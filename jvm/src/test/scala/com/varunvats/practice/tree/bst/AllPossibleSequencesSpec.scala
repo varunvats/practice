e55ea8777318ffbe58adccba9cd09fe8da9b352c
Varun Vats <varun.vats@here.com>
2016-03-02 10:10:43 -0600

    BST - All Possible Sequences: Complete pending tests.

--- jvm/src/test/scala/com/varunvats/practice/tree/bst/AllPossibleSequencesSpec.scala
+++ jvm/src/test/scala/com/varunvats/practice/tree/bst/AllPossibleSequencesSpec.scala
@@ -32,7 +32,37 @@ class AllPossibleSequencesSpec extends UnitSpec {
         AllPossibleSequences(tree) should contain theSameElementsAs Seq(Seq(5, 7, 6))
       }
 
-      "the tree contains several nodes" in pending
+      "the tree contains several nodes" in {
+        val tree = BinaryTreeNode(10,
+          left = Some(BinaryTreeNode(7,
+            left = Some(BinaryTreeNode(6)),
+            right = Some(BinaryTreeNode(9)))),
+          right = Some(BinaryTreeNode(12,
+            right = Some(BinaryTreeNode(13)))))
+        val allPossibleSequences = Seq(
+          Seq(10, 7, 6, 9, 12, 13),
+          Seq(10, 7, 6, 12, 9, 13),
+          Seq(10, 7, 12, 6, 9, 13),
+          Seq(10, 12, 7, 6, 9, 13),
+          Seq(10, 7, 6, 12, 13, 9),
+          Seq(10, 7, 12, 6, 13, 9),
+          Seq(10, 12, 7, 6, 13, 9),
+          Seq(10, 7, 12, 13, 6, 9),
+          Seq(10, 12, 7, 13, 6, 9),
+          Seq(10, 12, 13, 7, 6, 9),
+          Seq(10, 7, 9, 6, 12, 13),
+          Seq(10, 7, 9, 12, 6, 13),
+          Seq(10, 7, 12, 9, 6, 13),
+          Seq(10, 12, 7, 9, 6, 13),
+          Seq(10, 7, 9, 12, 13, 6),
+          Seq(10, 7, 12, 9, 13, 6),
+          Seq(10, 12, 7, 9, 13, 6),
+          Seq(10, 7, 12, 13, 9, 6),
+          Seq(10, 12, 7, 13, 9, 6),
+          Seq(10, 12, 13, 7, 9, 6)
+        )
+        AllPossibleSequences(tree) should contain theSameElementsAs allPossibleSequences
+      }
     }
   }
 
