bd723028ce955f27b75c2115f5ade6bfe06828ef
Varun Vats <varun.vats@here.com>
2016-03-03 14:30:35 -0600

    BST - Paths with Sum: Complete pending tests.

--- jvm/src/test/scala/com/varunvats/practice/tree/PathsWithSumSpec.scala
+++ jvm/src/test/scala/com/varunvats/practice/tree/PathsWithSumSpec.scala
@@ -13,7 +13,7 @@ class PathsWithSumSpec extends UnitSpec {
         PathsWithSum(tree, 16) shouldBe 0
       }
 
-      "only one and its value is greater than the sum" in {
+      "only one node and its value is greater than the sum" in {
         val tree = BinaryTreeNode(16)
         PathsWithSum(tree, 10) shouldBe 0
       }
@@ -30,7 +30,81 @@ class PathsWithSumSpec extends UnitSpec {
     }
 
     "be 1" when theTreeContains {
-      "a node with the value of sum and no other paths sum to this value" in pending
+      "a node with the value of sum and no other paths sum to this value (tree contains only +ve numbers)" in {
+        val tree = BinaryTreeNode(10,
+          left = Some(BinaryTreeNode(7,
+            left = Some(BinaryTreeNode(6)),
+            right = Some(BinaryTreeNode(9)))),
+          right = Some(BinaryTreeNode(12,
+            right = Some(BinaryTreeNode(13)))))
+        PathsWithSum(tree, 7) shouldBe 1
+      }
+
+      "no node with the value of sum and one path that sums to this value (tree has +ve and -ve numbers)" in {
+        val tree = BinaryTreeNode(-3,
+          left = Some(BinaryTreeNode(-6,
+            left = Some(BinaryTreeNode(-7)),
+            right = Some(BinaryTreeNode(-4)))),
+          right = Some(BinaryTreeNode(-1,
+            left = Some(BinaryTreeNode(-1,
+              left = Some(BinaryTreeNode(-2)))),
+            right = Some(BinaryTreeNode(3,
+              left = Some(BinaryTreeNode(2)),
+              right = Some(BinaryTreeNode(4)))))))
+        PathsWithSum(tree, -9) shouldBe 1
+      }
+    }
+
+    "be 2" when theTreeContains {
+      "two nodes with the value of sum and no other paths sum to this value (tree contains only +ve numbers)" in {
+        val tree = BinaryTreeNode(10,
+          left = Some(BinaryTreeNode(7,
+            left = Some(BinaryTreeNode(7)),
+            right = Some(BinaryTreeNode(9)))),
+          right = Some(BinaryTreeNode(12,
+            right = Some(BinaryTreeNode(13)))))
+        PathsWithSum(tree, 7) shouldBe 2
+      }
+
+      "one node with the value of sum and one path that sums to this value (tree contains only +ve numbers)" in {
+        val tree = BinaryTreeNode(10,
+          left = Some(BinaryTreeNode(7,
+            left = Some(BinaryTreeNode(6)),
+            right = Some(BinaryTreeNode(9)))),
+          right = Some(BinaryTreeNode(12,
+            right = Some(BinaryTreeNode(13)))))
+        PathsWithSum(tree, 13) shouldBe 2
+      }
+
+      "no nodes have the given sum but two paths that sum to this value (tree contains +ve and -ve numbers)" in {
+        val tree = BinaryTreeNode(-3,
+          left = Some(BinaryTreeNode(-6,
+            left = Some(BinaryTreeNode(-7)),
+            right = Some(BinaryTreeNode(-4)))),
+          right = Some(BinaryTreeNode(-1,
+            left = Some(BinaryTreeNode(-1,
+              left = Some(BinaryTreeNode(-2)))),
+            right = Some(BinaryTreeNode(3,
+              left = Some(BinaryTreeNode(2)),
+              right = Some(BinaryTreeNode(4)))))))
+        PathsWithSum(tree, -13) shouldBe 2
+      }
+    }
+
+    "be 3" when theTreeContains {
+      "one node with the value of sum and two paths that sum to this value (tree contains +ve and -ve numbers)" in {
+        val tree = BinaryTreeNode(-3,
+          left = Some(BinaryTreeNode(-6,
+            left = Some(BinaryTreeNode(-7)),
+            right = Some(BinaryTreeNode(-4)))),
+          right = Some(BinaryTreeNode(-1,
+            left = Some(BinaryTreeNode(-1,
+              left = Some(BinaryTreeNode(-2)))),
+            right = Some(BinaryTreeNode(3,
+              left = Some(BinaryTreeNode(2)),
+              right = Some(BinaryTreeNode(4)))))))
+        PathsWithSum(tree, -4) shouldBe 3
+      }
     }
   }
 
